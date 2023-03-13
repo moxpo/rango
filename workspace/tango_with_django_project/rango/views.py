28bf2ff1b585276403f77f996c62bb147c82a896
Moxpo <gregor.fiddes@gmail.com>
2023-03-13 22:43:05 +0000

    static / images not loading

--- workspace/tango_with_django_project/rango/views.py
+++ workspace/tango_with_django_project/rango/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    response = HttpResponse("Rango says hey there partner! Rango says here is the <a href='/rango/about/'>About</a> page.")
-    return response
+#    response = HttpResponse("Rango says hey there partner! Rango says here is the <a href='/rango/about/'>About</a> page.")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 def about(request):
     response = HttpResponse("Rango says here is the about page. Go back to the <a href='/rango/'>Index</a> page.")
     return response
