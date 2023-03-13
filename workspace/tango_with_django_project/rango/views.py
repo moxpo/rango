71a6cf4d76b4724ade554ba0afb6840205375306
Moxpo <gregor.fiddes@gmail.com>
2023-03-13 21:13:48 +0000

    Chapter 3

--- workspace/tango_with_django_project/rango/views.py
+++ workspace/tango_with_django_project/rango/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    response = HttpResponse("Rango says hey there partner! Visit the <a href='/rango/about/'>About</a> page.")
+    response = HttpResponse("Rango says hey there partner! Rango says here is the <a href='/rango/about/'>About</a> page.")
     return response
 def about(request):
-    response = HttpResponse("This is the about page. Go back to the <a href='/rango/'>Index</a> page.")
+    response = HttpResponse("Rango says here is the about page. Go back to the <a href='/rango/'>Index</a> page.")
     return response
