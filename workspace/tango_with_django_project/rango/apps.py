f1a6bc7e8cef4f6163b0441635a489c84c1c0804
Moxpo <gregor.fiddes@gmail.com>
2023-03-17 15:03:55 +0000

    Chapter 5

--- workspace/tango_with_django_project/rango/apps.py
+++ workspace/tango_with_django_project/rango/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
 
+from django.contrib import admin
+from rango.models import Category, Page
+
 
 class RangoConfig(AppConfig):
     name = 'rango'
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'views')
+    ordering = ('title',)
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
