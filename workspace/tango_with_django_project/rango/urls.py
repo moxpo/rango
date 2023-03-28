71416d3e185eb78fdf13314f3678b9b4b7868de0
Moxpo <gregor.fiddes@gmail.com>
2023-03-28 14:07:34 +0000

    Chapter 6

--- workspace/tango_with_django_project/rango/urls.py
+++ workspace/tango_with_django_project/rango/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rango.views import about
 from rango import views
+from rango.views import show_page
+
 app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('category/<slug:category_name_slug>/',
-        views.show_category, name='show_category'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    path('page/<slug:page_slug>/', views.show_page, name='show_page'),
 ]
--- workspace/tango_with_django_project/rango/views.py
+++ workspace/tango_with_django_project/rango/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
+
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     return render(request, 'rango/index.html', context=context_dict)
 
@@ -27,3 +30,13 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
+
+def show_page(request, page_slug):
+    try:
+        page = Page.objects.get(slug=page_slug)
+        context_dict = {'page': page}
+    except Page.DoesNotExist:
+        context_dict = {'page': None}
+    return render(request, 'rango/page.html', context=context_dict)
+
+
